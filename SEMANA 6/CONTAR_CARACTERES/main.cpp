f796f687b5ff043459fc2a4274f76c12b170adf5
Alumno B303 <Alumno-B303@UCSP.AULAB>
2025-09-17 08:27:12 -0500

    17/09

---
+++ SEMANA 6/CONTAR_CARACTERES/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+    /*string cad="Ciencia de la computación";
+    char car='i';
+    int cant=0;
+    for(int i=0; i<cad.lenght();i++){
+        if (cad.at(i)==car){
+            cant++
+        }
+    }
+    cout << cout << endl;*/
+
+
+
+    //dada una cadena veifcarr si dicha cadena es un palindrmo
+
+/*bool palindromo(string cad) {
+    int n = cad.length();
+    for (int i = 0; i < n/2 ; i++) {
+        if (cad.at(i) != cad.at(n - 1 - i)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    string cad = "hol a";
+    if (palindromo(cad))
+        cout << "Es un palíndromo" << endl;
+    else
+        cout << "No es un palíndromo" << endl;
+
+    return 0;
+}**/
+
+//implementar una funcion que reciba dos valores e intercambie sus funciones
+
+int a=30,b=10;
+
+for (a!=b;)
+
+void intercambio
+
+
+
+
+
+
